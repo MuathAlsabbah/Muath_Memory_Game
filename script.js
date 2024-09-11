8bb11bdf09ae96eeb6c049c87c98914e02d37bce
MuathAlsabbah <muath.alsabbah@gmail.com>
2024-09-11 16:24:23 +0300

    Memory Game

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 let gameActive = true
 const selectTracker = []
 const maxSelect = 2
+const board = Array(16).fill(null)
 
 //Getting all the cards
 const cards = document.body.querySelectorAll('.block')
@@ -11,10 +12,10 @@ const cardImages = [
   'https://easy-peasy.ai/cdn-cgi/image/quality=80,format=auto,width=700/https://fdczvxmwwjwpwbeeqcth.supabase.co/storage/v1/object/public/images/139484d9-a493-4e8a-9ef1-453a7fd92252/120d3586-28b2-4c3c-a244-af2d0b6cbf7b.png',
   'https://preview.redd.it/m4r7p5k99yt81.png?auto=webp&s=82932526b051ab9a61b71f7f95afbe4a655c9ba5',
   'https://geekygals.net/wp-content/uploads/2016/02/deadpool-thumbs-up.jpg',
-  'https://i.pinimg.com/736x/33/c5/a7/33c5a74a1af4cb6b7ff58bd4d77d4acc.jpg',
+  'https://i.insider.com/5bc07a09bde70f74b26c8f87?width=700',
   'https://i.pinimg.com/736x/5c/0d/b7/5c0db77f357dfbb8554b202c15a6abd1.jpg',
   'https://wallpapercave.com/wp/wp5187201.jpg',
-  'https://i.kym-cdn.com/photos/images/newsfeed/001/930/227/6eb.png',
+  'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/de31c88e-87bd-48b5-afc9-1b9b2164e7ec/dfh0bdm-ec91e132-0855-4f13-bc6f-5822716fb527.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2RlMzFjODhlLTg3YmQtNDhiNS1hZmM5LTFiOWIyMTY0ZTdlY1wvZGZoMGJkbS1lYzkxZTEzMi0wODU1LTRmMTMtYmM2Zi01ODIyNzE2ZmI1MjcuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.BIixyia_jwh0nCbd2MPfEkNPlbhPizbAg4RDFBrXHWw',
   'https://i.dailymail.co.uk/1s/2021/12/04/10/51329129-10274461-image-a-46_1638613409495.jpg'
 ]
 
@@ -73,15 +74,27 @@ const cardMatching = (column, row) => {
     }
   }
 }
+
+let matchesFound = 0
+
 //checkMatch function
 const checkMatch = () => {
+  const totalPairs = cardImages.length
   const [firstSelect, secondSelect] = selectTracker
   const firstImage = firstSelect.image
   const secondImage = secondSelect.image
 
-  if (firstSelect === secondImage) {
+  if (firstImage === secondImage) {
     console.log('match found')
+    matchesFound++
     selectTracker.length = 0
+
+    console.log(matchesFound)
+    console.log(totalPairs)
+    if (matchesFound === totalPairs) {
+      alert('Well done champ. But you could surely be faster...')
+      clearInterval(timeInterval)
+    }
   } else {
     console.log('no match')
 
@@ -90,34 +103,57 @@ const checkMatch = () => {
       secondSelect.block.style.backgroundImage = ''
       selectTracker.length = 0
       gameActive = true
-    }, 1000)
+    }, 500)
   }
 }
 
-//Creating a player message element on top
-const messageElement = document.createElement('div')
-messageElement.id = 'message'
-document.body.insertBefore(messageElement, document.body.querySelector('main'))
-
-//Creating the game reset button
 const resetButton = document.createElement('button')
+resetButton.textContent = 'Reset Button'
 resetButton.id = 'reset'
-document.body.insertBefore(resetButton, document.body.querySelector('main'))
+document.body.insertBefore(resetButton, document.querySelector('main'))
 
-resetButton.addEventListener('click', () => {
-  selectTracker.length = 0
-  gameActive = true
-  assigningMatches()
+const resetFunction = () => {
+  location.reload()
+}
+
+resetButton.addEventListener('click', resetFunction)
+
+let seconds = 25
+let timeInterval = setInterval(() => {
+  seconds--
+  if (seconds === 0) {
+    gameOver()
+  }
+  timer.innerHTML = `Time Remaining: ${seconds}`
+}, 1000)
+const gameOver = () => {
+  clearInterval(timeInterval)
+  alert('What an absolute sloth! 🤡 Lock in bro 😭')
+}
+const timer = document.createElement('div')
+timer.id = 'timer'
+
+document.body.insertBefore(timer, document.querySelector('main'))
+
+// Get the audio element by its ID
+const backgroundMusic = document.getElementById('background-music')
+
+const pauseButton = document.createElement('button')
+pauseButton.id = 'pauseButton'
+pauseButton.textContent = 'Pause Music'
+document.body.insertBefore(pauseButton, document.querySelector('#reset'))
+
+const playButton = document.createElement('button')
+playButton.id = 'playButton'
+playButton.textContent = 'Play Music'
+document.body.insertBefore(playButton, document.querySelector('#pauseButton'))
+
+playButton.addEventListener('click', () => {
+  backgroundMusic.play()
 })
 
-//Creating the game reset button
-const startGame = document.createElement('button')
-resetButton.id = 'start'
-document.body.insertBefore(startGame, document.body.querySelector('main'))
-
-startGame.addEventListener('click', () => {
-  selectTracker.length = 0
-  gameActive = true
+pauseButton.addEventListener('click', () => {
+  backgroundMusic.pause()
 })
 
 assigningMatches()
